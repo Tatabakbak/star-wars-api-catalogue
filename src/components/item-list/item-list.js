32f3e44c47f3c7c33f6d968ee746eec16859005d
Tata <tats.dev@gmail.com>
2019-11-06 01:13:37 +0300

    item list component filled

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,19 +1,52 @@
 import React, {Component} from 'react';
 import './item-list.css';
+import SwapiService from "../../services/swapi";
+import Spinner from "../spinner/spinner";
 
 export default class ItemList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            peopleList: null
+        };
+        this.swapiService = new SwapiService();
+    }
+
+    componentDidMount() {
+        this.swapiService.getAllPeople()
+            .then((itemList) => {
+                this.setState({
+                    itemList
+                })
+            });
+    }
+
+    onItemSelected(id) {
+        this.props.onItemSelected(id);
+    }
+
+    renderItems(arr) {
+        return arr.map(({id, name}) => {
+            return (
+                <li className="list-group-item bg-warning"
+                    key={id}
+                    onClick={() => this.onItemSelected(id)}>
+                    {name}
+                </li>
+            );
+        });
+    }
+
     render() {
+        const {itemList} = this.state;
+        if (!itemList) {
+            return <Spinner color={this.props.spinnerColor}/>;
+        }
+
         return (
             <ul className="item-list">
-                <li className="list-group-item bg-warning">
-                    Person1
-                </li>
-                <li className="list-group-item bg-warning">
-                    Person2
-                </li>
-                <li className="list-group-item bg-warning">
-                    Person3
-                </li>
+                {this.renderItems(itemList)}
             </ul>
         );
     };
