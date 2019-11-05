06eb725174bd5d862f6e5b77833cdf81cdb070a4
Tata <tats.dev@gmail.com>
2019-11-06 01:09:52 +0300

    spinner added

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import './spinner.css';
 
-const Spinner = () => {
+const Spinner = ({color}) => {
+
+    const userColor = color ? color : 'green';
+
+    const lightColors = {
+        green: '#8dd5c1',
+        blue: '#B4E1ED',
+        yellow: '#FFDB90'
+    };
+
+    const darkColors = {
+        green: '#54aa92',
+        blue: '#3BB3CC',
+        yellow: '#F4B52C'
+    };
+
+    const lightStyle = {
+        background: lightColors[userColor]
+    };
+
+    const darkStyle = {
+        background: darkColors[userColor]
+    };
+
     return(
         <div className="d-flex justify-content-center w-100">
             <div className="lds-flickr">
-                <div></div>
-                <div></div>
-                <div></div>
+                <div style={lightStyle}></div>
+                <div style={darkStyle}></div>
+                <div style={lightStyle}></div>
             </div>
         </div>
     );
@@ -15,3 +38,6 @@ const Spinner = () => {
 
 export default Spinner;
 
+//blue
+//#B4E1ED light
+//#4FB7CC dark
