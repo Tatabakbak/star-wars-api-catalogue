7f05e5b9aeb5a9029ecfe6ced0210dd7d3a5ead9
Tata <tats.dev@gmail.com>
2019-11-11 03:11:24 +0300

    routing

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,8 +6,10 @@ import SwapiService from "../../services/swapi";
 import ErrorBoundry from "../error-boundry/error-boundry";
 import {SwapiServiceProvider} from "../swapi-service-context";
 import DummySwapiService from "../../services/dummy-swapi";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import {PeoplePage, PlanetsPage, StarshipsPage} from "../pages";
+import {StarshipDetails} from "../sw-components";
 
 export default class App extends Component {
 
@@ -45,22 +47,31 @@ export default class App extends Component {
         return (
             <ErrorBoundry>
                 <SwapiServiceProvider value={swapiService}>
-                    <div>
-                        <Header onAPIChange={this.onAPIChange}/>
-                        <div className="page-wrapper">
-                            {randPlanet}
-                            {/*<button*/}
-                            {/*    className='btn btn-success toggle-planet-btn'*/}
-                            {/*    onClick={this.toggleRandomPlanet}>*/}
-                            {/*    Toggle random planet*/}
-                            {/*</button>*/}
-                            {/*<ErrorButton/>*/}
-                            <PeoplePage/>
-                            <PlanetsPage/>
-                            <StarshipsPage/>
+                    <Router>
+                        <div>
+                            <Header onAPIChange={this.onAPIChange}/>
+                            <div className="page-wrapper">
+                                {randPlanet}
+                                <Route path="/" render={() => <h2>Welcome to StarWars Catalogue</h2>} exact/>
+                                <Route path="/people" component={PeoplePage}/>
+                                <Route path="/planets" component={PlanetsPage}/>
+                                <Route path="/starships" component={StarshipsPage} exact/>
+                                <Route path="/starships/:id"
+                                       render={({match}) => {
+                                           const {id} = match.params;
+                                           return <StarshipDetails itemId={id}/>
+                                       }} exact/>
 
+                                {/*<button*/}
+                                {/*    className='btn btn-success toggle-planet-btn'*/}
+                                {/*    onClick={this.toggleRandomPlanet}>*/}
+                                {/*    Toggle random planet*/}
+                                {/*</button>*/}
+                                {/*<ErrorButton/>*/}
+
+                            </div>
                         </div>
-                    </div>
+                    </Router>
                 </SwapiServiceProvider>
             </ErrorBoundry>
         );
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './header.css';
 
 const Header = ({onAPIChange}) => {
     return(
-
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a className="navbar-brand" href="./index.js">Star Wars Catalogue</a>
+            <Link className="navbar-brand" to="/">Star Wars Catalogue</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
                     aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
@@ -13,13 +13,13 @@ const Header = ({onAPIChange}) => {
             <div className="collapse navbar-collapse" id="navbarColor01">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
-                        <a className="nav-link" href="./index.js">People</a>
+                        <Link className="nav-link" to="/people">People</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="./index.js">Planets</a>
+                        <Link className="nav-link" to="/planets">Planets</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="./index.js">Starships</a>
+                        <Link className="nav-link" to="/starships">Starships</Link>
                     </li>
                 </ul>
                 <button
